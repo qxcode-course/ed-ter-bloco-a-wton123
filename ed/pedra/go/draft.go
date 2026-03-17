2968aa8a6e62ed52f6ce9e598f179e8c4302a6cd
wton123 <wl3968968@gmail.com>
2026-03-17 01:45:57 +0000

    pedranalua

--- ed/pedra/go/draft.go
+++ ed/pedra/go/draft.go
@@ -1,5 +1,39 @@
 package main
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+    var n int
+    fmt.Scan(&n)
+
+    var ant, p int
+    var ind = -1
+
+    for i := 0; i < n; i++ {
+        var a, b int
+        fmt.Scan(&a, &b)
+
+        if a < 10 || b < 10 {
+            continue
+        }
+
+        if a < b {
+            p = b - a
+        } else {
+            p = a - b
+        }
+
+        if ind == -1 {
+            ant = p
+            ind = i
+        } else if p < ant {
+            ant = p
+            ind = i
+        }
+    }
+
+    if ind == -1 {
+        fmt.Println("sem ganhador")
+    } else {
+        fmt.Println(ind)
+    }
 }
