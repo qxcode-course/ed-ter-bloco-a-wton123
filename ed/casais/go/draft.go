5f2419179b971f644c020b8169e35b5c00672ca1
wton123 <wl3968968@gmail.com>
2026-03-17 14:40:16 +0000

    animaissalvos

--- ed/casais/go/draft.go
+++ ed/casais/go/draft.go
@@ -1,6 +1,33 @@
 package main
+
 import "fmt"
+
 func main() {
-    var n int
-    fmt.Println("qxcode")
+	var n, q, ani int
+	fmt.Scan(&n)
+
+	a := make([]int, n)         
+	b := make(map[int]bool)  
+
+	for i := 0; i < n; i++ {
+		fmt.Scan(&ani)
+		a[i] = ani
+	}
+
+	for i := 0; i < n; i++ {
+		if b[i] {
+			continue 
+		}
+
+		for j := i + 1; j < n; j++ {
+			if !b[j] && a[i]+a[j] == 0 { 
+				q++
+				b[i] = true
+				b[j] = true
+				break
+			}
+		}
+	}
+
+	fmt.Println(q)
 }
