b5df82d9123aaf515f286ffbe8dac28d32276e57
wton123 <wl3968968@gmail.com>
2026-03-31 02:12:40 +0000

    tentei

--- ed/manipulacao/go/shell.go
+++ ed/manipulacao/go/shell.go
@@ -1,3 +1,13 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func getMen(vet []int) []int {
 	var res []int
 	for _, x := range vet {
@@ -81,14 +91,85 @@ func repeated(vet []int) []int {
 	}
 
 	var res []int
-
 	for _, x := range vet {
 		if count[x] > 1 {
-			res = append(res, x)
+			count[x]--
+			if count[x] > 0 {
+				res = append(res, x)
+			}
 		}
 	}
+
+	
+	for i := 0; i < len(res); i++ {
+		for j := i + 1; j < len(res); j++ {
+			if res[i] > res[j] {
+				res[i], res[j] = res[j], res[i]
+			}
+		}
+	}
+
 	return res
 }
 
+func printVec(vet []int) {
+	fmt.Print("[")
+	for i, val := range vet {
+		if i > 0 {
+			fmt.Print(", ")
+		}
+		fmt.Print(val)
+	}
+	fmt.Println("]")
+}
 
+func str2vet(s string) []int {
+	if s == "[]" {
+		return nil
+	}
+	s = s[1 : len(s)-1]
+	parts := strings.Split(s, ",")
+	var vet []int
+	for _, part := range parts {
+		n, _ := strconv.Atoi(part)
+		vet = append(vet, n)
+	}
+	return vet
+}
 
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		if !scanner.Scan() {
+			break
+		}
+		fmt.Print("$")
+		line := scanner.Text()
+		args := strings.Split(line, " ")
+		fmt.Println(line)
+
+		switch args[0] {
+		case "get_men":
+			printVec(getMen(str2vet(args[1])))
+		case "get_calm_women":
+			printVec(getCalmWomen(str2vet(args[1])))
+		case "sort":
+			printVec(sortVet(str2vet(args[1])))
+		case "sort_stress":
+			printVec(sortStress(str2vet(args[1])))
+		case "reverse":
+			array := str2vet(args[1])
+			other := reverse(array)
+			printVec(array)
+			printVec(other)
+		case "unique":
+			printVec(unique(str2vet(args[1])))
+		case "repeated":
+			printVec(repeated(str2vet(args[1])))
+		case "end":
+		
+			return
+		}
+	}
+}
