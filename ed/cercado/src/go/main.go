cce19f32e88c0445bd14fa6501bfad079ef857a7
wton123 <wl3968968@gmail.com>
2026-07-04 22:22:18 +0000

    cerca

--- ed/cercado/src/go/main.go
+++ ed/cercado/src/go/main.go
@@ -6,9 +6,60 @@ import (
 	"os"
 )
 
+func calcilhas(board [][]byte, visita [][]bool, i, j int){
+	if i < 0 || j < 0 || i >= len(board) || j >= len(board[0]){
+		return 
+	}
+	if board[i][j] == 'X' || visita[i][j]{
+		return
+	}
+
+	visita[i][j] = true
+
+	calcilhas(board,visita ,i+1 ,j )
+	calcilhas(board,visita, i ,j+1 )
+	calcilhas(board,visita, i-1 ,j )
+	calcilhas(board,visita, i ,j-1 )
+
+}
 // NÃO ALTERE A ASSINATURA DA FUNÇÃO solve
 func solve(board [][]byte) {
-	_ = board
+	if len(board) == 0 {
+		return 
+	}
+	n := len(board)
+	m := len(board[0])
+	visita := make([][]bool, n)
+	for i := range visita{
+		visita[i] = make([]bool, m)
+	}
+
+	for i := 0; i < n ; i++{
+		if board[i][0] == 'O'{
+			calcilhas(board, visita ,i ,0)
+		}
+		if board[i][m-1] == 'O'{
+			calcilhas(board, visita ,i ,m-1)
+		}
+	}
+
+	for j := 0; j < n ; j++{
+		if board[0][j] == 'O'{
+			calcilhas(board, visita ,0 ,j)
+		}
+		if board[n-1][j] == 'O'{
+			calcilhas(board, visita ,n-1 ,j)
+		}
+	}
+
+	for i := 0; i < n ; i++{
+		for j := 0; j < m ; j++{
+		if board[i][j] == 'O' && !visita[i][j]{
+			board[i][j] = 'X'
+		}
+		
+	}
+	}
 }
 
 // NÃO ALTERE A MAIN
