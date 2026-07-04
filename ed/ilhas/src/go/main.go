c32ae1e7b672866745deab5aca41162838d72a19
wton123 <wl3968968@gmail.com>
2026-07-04 21:26:52 +0000

    ok ilhas

--- ed/ilhas/src/go/main.go
+++ ed/ilhas/src/go/main.go
@@ -5,13 +5,41 @@ import (
 	"fmt"
 	"os"
 )
+func calcilhas(grid [][]byte, i, j int){
+	if i < 0 || j < 0 || i >= len(grid) || j >= len(grid[0]){
+		return 
+	}
+	if grid[i][j] == '0'{
+		return
+	}
+
+	grid[i][j] = '0'
+	calcilhas(grid, i+1 ,j )
+	calcilhas(grid, i ,j+1 )
+	calcilhas(grid, i-1 ,j )
+	calcilhas(grid, i ,j-1 )
+
+}
 
 // Não modifique a assinatura da função numIslands
 // Ela é a função que será chamada no LeetCode para resolver o problema
 func numIslands(grid [][]byte) int {
 	//
-	_ := grid
-	return 0
+	if len(grid) == 0 {
+		return 0
+	}
+	n := 0
+	for i := 0; i < len(grid); i++{
+		for j := 0; j < len(grid); j++{
+			if grid[i][j] == '1'{
+						calcilhas(grid,i,j)
+						n++
+			}
+		
+
+	}
+	}
+	return n
 }
 
 // Não modifique a função main
